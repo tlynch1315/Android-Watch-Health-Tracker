0a6e6bee2a329c0a51e7a72e5599dfa00cf98531
Tommy Lynch <tlynch1315@gmail.com>
2017-12-15 00:59:33 -0500

    in checkUser change > to <

--- wear/src/main/java/com/lynch/accelerometer/MainActivity.java
+++ wear/src/main/java/com/lynch/accelerometer/MainActivity.java
@@ -1,11 +1,11 @@
 package com.lynch.accelerometer;
 
-import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import java.util.Collections;
 import java.util.Vector;
 
-public class MainActivity extends Activity implements SensorEventListener{
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
     private TextView mTextView;
     TextView acceleration;
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements SensorEventListener{
     SensorManager sensorManager;
     Vector<Float> heartRateVals = new Vector<Float>();
     Vector<Float> stepCountVals = new Vector<Float>();
-    boolean updated;
+    boolean updated = false;
     Float mean1 = null;
     Float std1 = null;
     Float mean2 = null;
@@ -36,10 +36,8 @@ public class MainActivity extends Activity implements SensorEventListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        heartRateVals.removeAllElements();
-        stepCountVals.removeAllElements();
-        updated = false;
         super.onCreate(savedInstanceState);
+        setAmbientEnabled();
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -48,46 +46,42 @@ public class MainActivity extends Activity implements SensorEventListener{
                 mTextView = (TextView) stub.findViewById(R.id.acceleration);
             }
         });
+        if (updated == false) {
+            Log.i("Updated", "= false");
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        //stepcounter   = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
-        boolean hasBodySensor = sensorManager.getDefaultSensor(34, true) != null;
-        if (hasBodySensor){
-            Log.i("Has Body Sensor", "Confirmed");
-        }
-        else{
-            Log.i("Does not have", "body sensor");
-        }
-        /*if (accelerometer == null){
-            List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-            for (Sensor sensor1 : sensors){
-                Log.i(TAG, sensor1.getName() + ": ");
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            //stepcounter   = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+
+            /*
+            used this to test if the watch had a sensor to detect if being worn
+            boolean hasBodySensor = sensorManager.getDefaultSensor(34, true) != null;
+            if (hasBodySensor) {
+                Log.i("Has Body Sensor", "Confirmed");
+            } else {
+                Log.i("Does not have", "body sensor");
             }
-        }*/
-        sensorManager.registerListener(this, stepcounter, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            */
 
-        acceleration = (TextView)findViewById(R.id.acceleration);
+            sensorManager.registerListener(this, stepcounter, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+            acceleration = (TextView) findViewById(R.id.acceleration);
+            updated = true;
+        }
 
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        //Log.d("SENSOR CHANGED", "onSensorChanged is being executed");
-        //updateDisplay();
         if (sensorEvent.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-            //if (sensorEvent.values[0] != 0) {
             stepCountVals.addElement(sensorEvent.values[0]);
             Log.i("Step Count", Float.toString(sensorEvent.values[0]));
-            //}
         }
         if (sensorEvent.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-            //if (sensorEvent.values[0] != 0) {
             heartRateVals.addElement(sensorEvent.values[0]);
             Log.i("HEART RATE", Float.toString(sensorEvent.values[0]));
             Log.i("HR sample size: ", Integer.toString(heartRateVals.size()));
-            //}
         }
         if (heartRateVals.size() > 100 ){
             if (checkNull()){
@@ -95,7 +89,6 @@ public class MainActivity extends Activity implements SensorEventListener{
                 stepCountVals.removeAllElements();
             }
             else {
-                //updated = true;
                 Log.i("Update Called", Integer.toString(heartRateVals.size()));
                 testUpdate();
             }
@@ -103,6 +96,10 @@ public class MainActivity extends Activity implements SensorEventListener{
     }
 
     public boolean checkNull(){
+        /*
+        function checks if the current sample of heart rate values is valid
+        valid if <= 75% 0's
+         */
         int count = 0;
         for(int i = 0; i < heartRateVals.size(); i++){
             if (heartRateVals.get(i) == 0){
@@ -118,16 +115,13 @@ public class MainActivity extends Activity implements SensorEventListener{
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
-    /*
-    @Override
-    public void onStop(){
-        super.onStop();
-        sensorManager.unregisterListener(this);
-    }*/
+
     public void testUpdate(){
+        /*
+        sort the heart rate vals to be able to get percentiles
+         */
         Collections.sort(heartRateVals);
         float twentyfive, fifty, seventyfive, ninetyfive;
-        Log.i("TEST", "BEFORE 25");
         int twentyfivepercent = (int)heartRateVals.size()/4;
         Log.i("25", Integer.toString(twentyfivepercent));
         twentyfive = heartRateVals.get(twentyfivepercent);
@@ -135,7 +129,7 @@ public class MainActivity extends Activity implements SensorEventListener{
         seventyfive = heartRateVals.get((int)((heartRateVals.size()/4) * 3));
         ninetyfive = heartRateVals.get((int)((heartRateVals.size()/20)*19));
 
-
+        // get sum of heart rate vals
         float sum = 0;
         float variance;
         float positive = 0;
@@ -146,16 +140,16 @@ public class MainActivity extends Activity implements SensorEventListener{
             }
         }
 
-        float average = sum/heartRateVals.size();
+        // get average, variance, and standard deviation
+        float average = sum/positive;
         acceleration = (TextView)findViewById(R.id.acceleration);
         acceleration.setText(Float.toString(average));
         float temp = 0;
-        //float positive = 0;
         for(float a : heartRateVals){
-            if (a > 0){
-                positive ++;
+            if (a > 0) {
+                positive++;
+                temp += (a - average) * (a - average);
             }
-            temp += (a-average)*(a-average);
         }
         variance = temp/positive;
         double a = variance;
@@ -166,19 +160,30 @@ public class MainActivity extends Activity implements SensorEventListener{
             std1 = (float) std;
         }
         else{
+            // not necessary to have mean2 and std2, I had it here
+            // prior to switching to majority checking
             Log.i("mean2 ", "updated");
             mean2 = average;
             std2 = (float) std;
+            // check if the current user is the same user or not
+            // since the mean1 was not null, it means there is a
+            // set of data for the original user
             checkSameUser();
         }
+        /*
+        LOGGING DIFFERENT PERCENTILES
         Log.i("Standard Deviation", Double.toString(std));
-        /*Log.i("25%: ", Integer.toString(twentyfive));
+        Log.i("25%: ", Integer.toString(twentyfive));
         Log.i("50%: ", Integer.toString(fifty));
         Log.i("75%: ", Integer.toString(seventyfive));
         Log.i("95%: ", Integer.toString(ninetyfive));*/
 
         // STEP COUNTER
+
         /*
+        THIS IS BASICALLY SAME THING AS THE ABOVE PROCESS EXCEPT FOR STEP COUNT
+        WE DID NOT USE STEP COUNT BECAUSE THE WINDOW OF TIME WE USED WAS TOO SMALL
+
         float twentyfiveSTEP, fiftySTEP, seventyfiveSTEP, ninetyfiveSTEP;
         Log.i("TEST", "BEFORE 25");
         int twentyfivepercentSTEP = (int)stepCountVals.size()/4;
@@ -218,16 +223,18 @@ public class MainActivity extends Activity implements SensorEventListener{
             std2STEP = (float) stdSTEP;
         }*/
         Log.i("Standard Deviation", Double.toString(std));
-        /*Log.i("25%: ", Integer.toString(twentyfive));
-        Log.i("50%: ", Integer.toString(fifty));
-        Log.i("75%: ", Integer.toString(seventyfive));
-        Log.i("95%: ", Integer.toString(ninetyfive));*/
+
         heartRateVals.removeAllElements();
         stepCountVals.removeAllElements();
         return;
 
     }
     public void checkSameUser(){
+        /*
+        This function determines if the user is the original user based on
+        majority checking of the positive values in a sample window being in
+        the range [avg - std, avd + std]
+         */
         Log.i("In checkSameUser", "function");
         Float lowerBound = mean1 - std1;
         Float upperBound = mean1 + std1;
@@ -244,13 +251,16 @@ public class MainActivity extends Activity implements SensorEventListener{
         if (count < positive/2){
             Log.i("diff user ", "detected");
             acceleration.setText("Different User Detected");
-            //sensorManager.unregisterListener(this);
         }
         else{
             Log.i("same user ", "detected");
             acceleration.setText("Same User");
+            /*
+            if you want to update the original user's data every
+            time that a the same user is detected:
             mean1 = mean2;
             std1 = std2;
+            */
         }
 
     }
